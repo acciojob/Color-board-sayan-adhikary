ae17f990f31940b897130f2716a29f7efe743672
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-06-04 20:31:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const container = document.getElementById("container");
+
+const colors = [
+    "#e74c3c",
+    "#8e44ad",
+    "#3498db",
+    "#f1c40f",
+    "#2ecc71"
+];
+
+// Create 800 boxes
+for (let i = 0; i < 800; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.addEventListener("mouseenter", () => {
+        const color = getRandomColor();
+
+        square.style.backgroundColor = color;
+
+        setTimeout(() => {
+            square.style.backgroundColor = "#1d1d1d";
+        }, 1000);
+    });
+
+    container.appendChild(square);
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
